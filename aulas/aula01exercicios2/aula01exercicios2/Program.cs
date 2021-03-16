4d791a34579cc4b2b9bb545f90f5fcced1b84d50
Rodrigo <rodrigo.roque82@gmail.com>
2021-03-16 18:32:42 -0300

    exemplo 2 concluido

---
+++ aulas/aula01exercicios2/aula01exercicios2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula01exercicios2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bidu = new cachorro();
+            bidu.nome = "Bidu da Silva";
+            bidu.raça = "Pit-bull";
+            bidu.genero = "macho";
+            bidu.idade = 3;
+
+            bidu.abanarrabo();
+            bidu.Latir();
+
+
+        }
+    }
+}
---
+++ aulas/aula01exercicios2/aula01exercicios2/cachorro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula01exercicios2
+{
+    class cachorro
+    {
+        public string nome;
+        public string raça;
+        public int idade;
+        public string genero;
+
+        public void abanarrabo()
+        {
+            Console.WriteLine("Rabinho balançando");
+            Console.ReadLine();
+        }
+        public void Latir()
+        {
+            Console.WriteLine("Au au au");
+            Console.ReadLine();
+        }
+
+    }
+}
