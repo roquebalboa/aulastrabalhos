a92e801c17819d2f7dbc406924e6fe829b3ea477
Rodrigo <rodrigo.roque82@gmail.com>
2021-03-16 10:46:00 -0300

    conclusão

--- aulas/aula01exercicios/aula01exercicios/Program.cs
+++ aulas/aula01exercicios/aula01exercicios/Program.cs
@@ -10,6 +10,14 @@ namespace aula01exercicios
     {
         static void Main(string[] args)
         {
+            //construtor "default"
+            Cachorro cao1 = new Cachorro();
+            var cao2 = new Cachorro();
+            Cachorro cao3 = new Cachorro();
+            var cao4 = new Cachorro();
+            var cao5 = new Cachorro();
+
+            Cachorro[] cachorros =  { cao1, cao2, cao3, cao4, cao5 };
         }
     }
 }
